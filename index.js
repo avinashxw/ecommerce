ee3c4026a61e0da79877d1816efb4914872b759c
Avinash Wadhawan <avinashxw@gmail.com>
2021-05-08 00:33:38 +0530

    enforce-code-style-using-eslint

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app= express()
+const app = express()
 
-app.get('/', function(req,res) {
-            return res.send('Hey, buddy!')
+app.get('/', function (req, res) {
+  return res.send('Hey, buddy!')
 })
 
-app.listen(3000, function(){
-                console.log('Server running at port 3000!');
+app.listen(3000, function () {
+  console.log('Server running at port 3000!')
 })
