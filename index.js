47ee0ad858369c820d2afdc30702abc05f17827c
Avinash Wadhawan <avinashxw@gmail.com>
2021-05-08 00:19:29 +0530

    auto-refresh-using-nodemon

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app = express()
+const app= express()
 
 app.get('/', function(req,res) {
-    return res.send('Hey buddy!')
+            return res.send('Hey, buddy!')
 })
 
 app.listen(3000, function(){
-    console.log('Server running at port 3000!')
+                console.log('Server running at port 3000!');
 })
