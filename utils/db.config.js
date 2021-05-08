47bec72603a485d3cdfcc1ce94446f7d4cb1fd2d
Avinash Wadhawan <avinashxw@gmail.com>
2021-05-09 01:02:58 +0530

    mongodb-connection

---
+++ utils/db.config.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/x-store',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to DB!')
+})
